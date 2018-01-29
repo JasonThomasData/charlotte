2a303430f47568cc643489e78cb7dc3600612163
john smith <jason.robert.thomas.123@gmail.com>
2018-01-29 19:22:31 +1100

    Updating install instructions to run

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@ cd lib
 # Install wiringPi dependency
 sudo apt-get purge wiringpi
 git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
-cd ../
-cd ../
+./wiringPi/build
 
-# Install the project in bin/
+git clone git@github.com:eranpeer/FakeIt.git
+cp FakeIt/single_header/standalone/fakeit.hpp ./
+
+git clone git@github.com:catchorg/Catch2.git
+cp Catch2/single_include/catch.hpp ./
+
+# Install the robot program and tests in bin
+cd ../
 make
 make clean
