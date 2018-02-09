5cded61877666605b18931880141ea503db8d090
John Smith <jason.robert.thomas.123@gmail.com>
2018-02-09 12:18:27 +0000

    Clearer file to install dependencies, build

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+# Install dependencies
 cd lib
+  sudo pacman -R wiringpi
+  git clone git://git.drogon.net/wiringPi
 
-# Install wiringPi dependency
-sudo apt-get purge wiringpi
-git clone git://git.drogon.net/wiringPi
-./wiringPi/build
+  cd wiringPi
+    ./build
+  cd ../
 
-git clone git@github.com:eranpeer/FakeIt.git
-cp FakeIt/single_header/standalone/fakeit.hpp ./
+  git clone git@github.com:eranpeer/FakeIt.git
+  cp FakeIt/single_header/standalone/fakeit.hpp ./
 
-git clone git@github.com:catchorg/Catch2.git
-cp Catch2/single_include/catch.hpp ./
+  git clone git@github.com:catchorg/Catch2.git
+  cp Catch2/single_include/catch.hpp ./
+cd ../
 
 # Install the robot program and tests in bin
-cd ../
 make
 make clean
