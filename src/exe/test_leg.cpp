182094948c6cd32fa44c8195caeab81bd1f1f360
John Smith <jason.robert.thomas.123@gmail.com>
2018-02-09 12:16:31 +0000

    Adding terminal args to test every leg

--- src/exe/test_leg.cpp
+++ src/exe/test_leg.cpp
@@ -2,10 +2,15 @@
 #include <wiringPi.h>
 #include <unistd.h>
 
+#include <sstream>
+#include <string>
+#include <stdexcept>
+
 #include "../servo/i_servo_driver.h"
 #include "../servo/servo_driver.h"
 #include "../leg/i_leg_driver.h"
 #include "../leg/leg_driver_02.h"
+#include "../leg/leg_driver_13.h"
 
 #include "../builder/builder.h"
 
@@ -18,23 +23,66 @@ int main(int argc, char* argv[])
 {
     if (wiringPiSetup() < 0) return 1;
 
-    IServoDriver* servo_0 = new ServoDriver(0);
-    IServoDriver* servo_1 = new ServoDriver(1);
-    IServoDriver* servo_2 = new ServoDriver(2);
+    if (argc < 2)
+    {
+        std::string message = "You should pass a leg number to test, like `./bin/test_leg 0`";
+        throw std::invalid_argument(message);
+    }
 
-    ILegDriver* leg_0 = new LegDriver_02(servo_0, servo_1, servo_2);
+    std::istringstream ss(argv[1]);
+    int leg_number;
+    ss >> leg_number;
 
-    leg_0->up();
+    IServoDriver* bottom_servo;
+    IServoDriver* middle_servo;
+    IServoDriver* top_servo;
+    ILegDriver* leg;
+
+    if (leg_number == 0)
+    {
+        bottom_servo = new ServoDriver(0);
+        middle_servo = new ServoDriver(1);
+        top_servo = new ServoDriver(2);
+        leg = new LegDriver_02(bottom_servo, middle_servo, top_servo);
+    }
+    else if (leg_number == 1)
+    {
+        bottom_servo = new ServoDriver(3);
+        middle_servo = new ServoDriver(4);
+        top_servo = new ServoDriver(5);
+        leg = new LegDriver_13(bottom_servo, middle_servo, top_servo);
+    }
+    else if (leg_number == 2)
+    {
+        bottom_servo = new ServoDriver(6);
+        middle_servo = new ServoDriver(7);
+        top_servo = new ServoDriver(8);
+        leg = new LegDriver_02(bottom_servo, middle_servo, top_servo);
+    }
+    else if (leg_number == 3)
+    {
+        bottom_servo = new ServoDriver(9);
+        middle_servo = new ServoDriver(10);
+        top_servo = new ServoDriver(11);
+        leg = new LegDriver_13(bottom_servo, middle_servo, top_servo);
+    }
+    else
+    {
+        std::string message = "You should enter a leg number 0-3, like `./bin/test_leg 2`";
+        throw std::invalid_argument(message);
+    }
+
+    leg->up();
     usleep(1000000);
-    leg_0->anti_clockwise();
+    leg->anti_clockwise();
     usleep(1000000);
-    leg_0->down();
+    leg->down();
     usleep(1000000);
-    leg_0->clockwise();
+    leg->clockwise();
     usleep(1000000);
-    leg_0->extend_flat();
+    leg->extend_flat();
     usleep(1000000);
-    leg_0->middle();
+    leg->middle();
     usleep(1000000);
 
     return 0;
